938a5f8a538a89855abb6a464e65efeac49dd043
Metehan Tırpan <111630803+black131@users.noreply.github.com>
2026-04-30 11:25:47 +0300

    Style Transfer with VGG19

--- main.py
+++ main.py
@@ -1,114 +1,137 @@
-"""
-problem tanimi: 
-pose estimation and action classification
-mediapipe: google tarafindan gelistirilen gercek zamanli yapay zeka temelli
-gorsel isleme kutuphanesi
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models, transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-plan program
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-kutuphane kurulumlari
-import libraries
-"""
-#import libraries
-import cv2 #opencv kutuphanesi
-import mediapipe as mp #mediapipe kutuphanesi
-import numpy as np #numpy kutuphanesi
+def load_image(image_path, max_size=400, shape=None):
+    """
+    image_path: resmin yolu
+    max_size: resmin maksimum pikseli
+    shape: stil resmiyle ayni boyuta eşitlemek için (H,W) tuple
+    """
+    image = Image.open(image_path).convert('RGB')
 
-#aci hesaplayan yardimci fonksiyon
-def calculate_angle(a,b,c): #a=(x,y)
-     """
-     Üç nokta arasindaki aciyi derece cinsinden hesaplar.
-     
-     """
-     a = np.array(a) #birinci nokta
-     b = np.array(b) #ikinci nokta
-     c = np.array(c) #ucuncu nokta
-     
-     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-     angle = np.abs(radians*180.0/np.pi)
-     
-     if angle > 180.0:
-         angle = 360-angle
-         
-     return angle
+    # Boyutlandırma
+    if shape is not None:
+        size = shape                    # stil ve içerik aynı HxW
+    else:
+        size = max(image.size)          # uzun kenarı al
+        if size > max_size:             # fazla uzunsa kırp
+            size = max_size
 
-#mediapipe modulleri tanimla
-mp_drowing=mp.solutions.drawing_utils #Video ya da görüntü üzerine çizim
-mp_pose=mp.solutions.pose #Poz modulu
+    # in_transform, if/else bloğunun DIŞINDA — aynı seviyede
+    in_transform = transforms.Compose([
+        transforms.Resize((size, size) if isinstance(size, int) else size),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
 
-#video dosyasını yükle
-cap=cv2.VideoCapture("squat_test1.avi") #video dosyasını yükle
-counter=0 #squat sayacı
-stage=None #squat pozisyonu
+    image = in_transform(image).unsqueeze(0)  # batch dimension ekle
+    return image.to(device)
 
-#basit kural tabanli poz tahmini gerceklestir
-def classify_pose(knee_angle):
-     """
-     diz acisina göre poz siniflandirma
-     """
-     if knee_angle<100:
-          return "squat"
-     elif 100<=knee_angle<=160:
-          return "lunging"
-     else:
-          return "standing"
-#print(calculate_angle((0,1),(0,0),(1,0)))
-#print(classify_pose(150))
+def im_convert(tensor):
+          """
+          Tensoru tekrardan 0-1 araligina ve (H,W,3) formuna getirir
+          Çünkü Matplotlib bu formatta görüntüleri gösterir
+          """
+          image=tensor.clone().detach().cpu().squeeze(0) 
+          #std ile carp ve ortalama ile topla yani ters normalizasyon yap 
+          image=image*torch.tensor([0.229, 0.224, 0.225]).view(3,1,1)
+          image=image*torch.tensor([0.485, 0.456, 0.406]).view(3,1,1)
+          image=image.clamp(0,1) #degerleri sifir ile 1 arasina sikistir.
+          return image.permute(1,2,0).numpy() #(C,H,W) -> (H,W,C)
 
-#pose modulunu olustur, yaptiklarimizi birlestir.
-with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
-     while cap.isOpened():
-          ret,frame=cap.read() #video karelerini oku
-          if not ret:
-               break #video bittiyse whiledan çık.
-          
-          #gorsel isleme adimlari
-          image=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) #BGR den RGB ye cevir
-          image.flags.writeable=False #gorsel uzerinde yazma izni kapat
-          
-          results=pose.process(image) #mediapipe ile poz tahmini yap
-          
-          image.flags.writeable=True #gorsel uzerinde yazma izni ac
-          image=cv2.cvtColor(image,cv2.COLOR_RGB2BGR) #RGB den BGR ye cevir opencv için
-          
-          try:
-               landmarks=results.pose_landmarks.landmark #pozlandirmalari al
-               
-               #sag kalca,diz,ayak bilegi
-               hip=(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
-                    landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y)
-               knee=(landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
-                     landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y)
-               ankle=(landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
-                      landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y)
-               
-               #diz acisini hesapla
-               angle=calculate_angle(hip,knee,ankle)
-               
-               #poz siniflandirma
-               current_pose=classify_pose(angle)
-               
-               #squat sayaci ayarlama
-               if angle < 90:
-                    stage="down"
-               if angle > 160 and stage=="down":
-                    stage="up"
-                    counter+=1
-               
-               #ekrana bilgileri yazdirma
-               cv2.putText(image,f"Knee Angle: {int(angle)} ",(10,60),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
-               cv2.putText(image,f"Squat sayisi: {counter}",(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
-               cv2.putText(image,f"Poz: {current_pose}",(10,90),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,255,255),2)
-          except:
-               pass
-          #Anahtar noktalar ve baglantilari cizme
-          if results.pose_landmarks:
-               mp_drowing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,
-                                        mp_drowing.DrawingSpec(color=(0,255,0),thickness=2,circle_radius=2),
-                                        mp_drowing.DrawingSpec(color=(255,0,0),thickness=2,circle_radius=2))
-          cv2.imshow("Pose classifier ile squat sayaci",image) #sonucu goster
-          if cv2.waitKey(10) & 0xFF == ord('q'):
-               break
-# Kaynaklari serbest birak
-cap.release()
-cv2.destroyAllWindows()
+#öznitelik cikarici model ve gram matrisi ile stil benzerligi olcutu
+def gram_matrix(tensor):
+    """
+    (C,H,W) -> (C, H*W) = A x A.T = gram matrix
+    """
+    _,d,h,w= tensor.size()
+    tensor=tensor.view(d,h*w) 
+    return torch.mm(tensor, tensor.t())        
+#öznitelik cikarici model = VGG19
+class VGGFeatures(nn.Module):
+    def __init__(self):
+        super(VGGFeatures, self).__init__()
+
+        self.vgg = models.vgg19(pretrained=True).features[:29].to(device).eval()
+
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+
+        self.layers = {
+            "0": "conv1_1",
+            "5": "conv2_1",
+            "10": "conv3_1",
+            "19": "conv4_1",
+            "21": "conv4_2",
+            "28": "conv5_1"
+        }
+
+    def forward(self, x):
+        features = {}
+        for name, layer in self.vgg._modules.items():
+            x = layer(x)
+            if name in self.layers:
+                features[self.layers[name]] = x
+        return features
+#stil transferi döngsünü tamamla
+def run_style_transfer(content_img, #cevirmek istedigimiz resim
+                       style_img,#stilini almak istedigimiz resim
+                       steps=2000,
+                       style_weight=1e6, #stil kaybi agirligi
+                       content_weight=1 #icerik kaybi katsayisi
+                       ):
+    #hedef degisken tensoru icerik gorselinden kopya oluşturmak
+    target=content_img.clone().requires_grad_(True).to(device)
+    optimizer=optim.Adam([target],lr=0.003)
+    model=VGGFeatures()
+    
+    for step in tqdm(range(steps)):
+        target_features=model(target) #guncel hedef
+        content_features=model(content_img) # sabit stil referansi
+        style_features=model(style_img)     # sabit stil referansi     
+        
+        content_loss=torch.mean((target_features['conv4_2']-content_features['conv4_2'])**2) #icerik kaybi
+        
+        #stil kaybi : secili her katman icin gram matrisi uzakligi
+        style_loss=0
+        for layer in ["conv1_1", "conv2_1", "conv3_1", "conv4_1", "conv5_1"]:
+            target_feature=target_features[layer]
+            style_feature=style_features[layer]
+            target_gram=gram_matrix(target_feature)
+            style_gram=gram_matrix(style_feature)
+            layer_loss=torch.mean((target_gram-style_gram)**2)
+            style_loss+=layer_loss
+        
+        #total loss: toplam kayip
+        total_loss=content_weight*content_loss+style_weight*style_loss
+        
+        optimizer.zero_grad()
+        total_loss.backward() #geri yayilim target tensoru (parametrelerini) güncelle
+        optimizer.step()
+        
+        if step% 500==0:
+            print(f"Step {step}, Total Loss: {total_loss.item():.2f}")
+    return target
+
+#uygulama
+content=load_image("cat.jpeg")
+style = load_image("style.jpg", shape=tuple(content.shape[-2:])) #stil görseli (HxW)
+
+output=run_style_transfer(content, style)
+
+#sonucu görsellestir
+plt.figure(figsize=(10,5))
+plt.imshow(im_convert(output))
+plt.title("Stil Transferi Sonucu")
+plt.axis("off")
+plt.show()       
